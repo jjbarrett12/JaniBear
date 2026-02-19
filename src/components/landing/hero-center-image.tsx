e381dc702f06de25a0cfccc134ff11415d4d6f91
Jason <jjbarrett12@gmail.com>
2026-02-19 01:44:39 -0700

    Deploy: 2026-02-19 08:44

--- src/components/landing/hero-center-image.tsx
+++ src/components/landing/hero-center-image.tsx
@@ -3,10 +3,11 @@
 import Image from 'next/image';
 import { useState } from 'react';
 
-/** Hero foreground: transparent devices image (ChatGPT Image ... or hero-devices-transparent.png) */
+/** Hero foreground: true transparent image first (True Transparent Hero.png), then fallbacks */
 const DEVICES_PATHS = [
-  '/ChatGPT%20Image%20Feb%2019%2C%202026%2C%2012_40_33%20AM.png',
+  '/True%20Transparent%20Hero.png',
   '/hero-devices-transparent.png',
+  '/ChatGPT%20Image%20Feb%2019%2C%202026%2C%2012_40_33%20AM.png',
   '/hero-devices.png',
   '/Laptop%20and%20phone%20display%20.png',
 ];
